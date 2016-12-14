e482e88775ea39ddea898f6afd1f6b1b91b8fb0a
mysorepr <prasad.mysore@osi.ca.gov>
2016-12-14 10:02:56 -0800

    added attorneyindexerjob

---
+++ src/main/java/gov/ca/cwds/jobs/AttorneyIndexerJob.java
@@ -0,0 +1,122 @@
+package gov.ca.cwds.jobs;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.SessionFactory;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+
+import gov.ca.cwds.dao.elasticsearch.ElasticsearchConfiguration;
+import gov.ca.cwds.dao.elasticsearch.ElasticsearchDao;
+import gov.ca.cwds.jobs.inject.CmsSessionFactory;
+import gov.ca.cwds.jobs.inject.JobsGuiceInjector;
+import gov.ca.cwds.rest.api.persistence.cms.Attorney;
+import gov.ca.cwds.rest.jdbi.cms.AttorneyDao;
+
+/**
+ * Job to load attornies from CMS into ElasticSearch
+ * 
+ * @author CWDS API Team
+ */
+public class AttorneyIndexerJob extends JobBasedOnLastSuccessfulRunTime {
+
+  private static final Logger LOGGER = LogManager.getLogger(PersonIndexerJob.class);
+
+  private static ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
+  private static ObjectMapper MAPPER = new ObjectMapper();
+
+  private AttorneyDao attorneyDao;
+  private ElasticsearchDao elasticsearchDao;
+
+
+  public AttorneyIndexerJob(AttorneyDao attorneyDao, ElasticsearchDao elasticsearchDao,
+      String lastJobRunTimeFilename) {
+    super(lastJobRunTimeFilename);
+    this.attorneyDao = attorneyDao;
+    this.elasticsearchDao = elasticsearchDao;
+  }
+
+  public static void main(String... args) throws Exception {
+    if (args.length != 2) {
+      throw new Error(
+          "Usage: java gov.ca.cwds.jobs.AttorneyIndexerJob esconfigFileLocation lastJobRunTimeFilename");
+    }
+
+    Injector injector = Guice.createInjector(new JobsGuiceInjector());
+    SessionFactory sessionFactory =
+        injector.getInstance(Key.get(SessionFactory.class, CmsSessionFactory.class));
+
+    AttorneyDao attorneyDao = new AttorneyDao(sessionFactory);
+
+    File file = new File(args[0]);
+    ElasticsearchConfiguration configuration =
+        YAML_MAPPER.readValue(file, ElasticsearchConfiguration.class);
+    ElasticsearchDao elasticsearchDao = new ElasticsearchDao(configuration);
+
+    AttorneyIndexerJob job = new AttorneyIndexerJob(attorneyDao, elasticsearchDao, args[1]);
+    try {
+      job.run();
+    } catch (JobsException e) {
+      LOGGER.error("Unable to complete job", e);
+    } finally {
+      sessionFactory.close();
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see gov.ca.cwds.jobs.JobBasedOnLastSuccessfulRunTime#_run(java.util.Date)
+   */
+  @Override
+  public Date _run(Date lastSuccessfulRunTime) {
+
+    try {
+      List<Attorney> results = attorneyDao.findAllUpdatedAfter(lastSuccessfulRunTime);
+      LOGGER.info(MessageFormat.format("Found {0} people to index", results.size()));
+      Date currentTime = new Date();
+      String nodate = new String();
+
+      elasticsearchDao.start();
+      for (Attorney attorney : results) {
+        gov.ca.cwds.rest.api.elasticsearch.ns.Person esPerson =
+            new gov.ca.cwds.rest.api.elasticsearch.ns.Person(attorney.getPrimaryKey(),
+                attorney.getFirstName(), attorney.getLastName(), "", // Gender
+                nodate, // DOB
+                "", // SSN
+                attorney.getClass().getName(), MAPPER.writeValueAsString(attorney));
+        indexDocument(esPerson);
+
+      }
+      LOGGER.info(MessageFormat.format("Indexed {0} people", results.size()));
+      LOGGER.info(MessageFormat.format("Updating last succesful run time to {0}",
+          DATE_FORMAT.format(currentTime)));
+      return currentTime;
+    } catch (IOException e) {
+      throw new JobsException("Could not parse configuration file", e);
+    } catch (Exception e) {
+      throw new JobsException(e);
+    } finally {
+      try {
+        elasticsearchDao.stop();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private void indexDocument(gov.ca.cwds.rest.api.elasticsearch.ns.Person person) throws Exception {
+    String document = MAPPER.writeValueAsString(person);
+    elasticsearchDao.index(document, person.getId().toString());
+  }
+}
