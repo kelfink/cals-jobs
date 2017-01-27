f559fad776138f1cfea8dfabec1c638e9ad23b48
David R. Smith <drs.code.monkey@gmail.com>
2017-01-27 10:51:19 -0800

    #138163381: Apache CLI to parse batch command line

--- src/main/java/gov/ca/cwds/jobs/ClientIndexerJob.java
+++ src/main/java/gov/ca/cwds/jobs/ClientIndexerJob.java
@@ -7,6 +7,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.bulk.BulkProcessor;
@@ -48,7 +56,7 @@ public class ClientIndexerJob extends JobBasedOnLastSuccessfulRunTime {
    * 
    * @param clientDao Client DAO
    * @param elasticsearchDao ElasticSearch DAO
-   * @param lastJobRunTimeFilename last batch run date in format yyyy-MM-dd HH:mm:ss
+   * @param lastJobRunTimeFilename last run date in format yyyy-MM-dd HH:mm:ss
    * @param mapper Jackson ObjectMapper
    * @param sessionFactory Hibernate session factory
    */
@@ -63,23 +71,79 @@ public class ClientIndexerJob extends JobBasedOnLastSuccessfulRunTime {
     this.sessionFactory = sessionFactory;
   }
 
-  public static void main(String... args) throws Exception {
-    if (args.length != 2) {
-      throw new JobsException("Usage: java " + ClientIndexerJob.class.getName()
-          + "<ES config file> <last job runtime file>");
+  public static final class JobOptions {
+    public String esConfigLoc;
+    public String lastRunLoc;
+    public boolean lastRunMode = true;
+  }
+
+  protected static Option makeOpt(String single, String longName, String description) {
+    return Option.builder(single).argName(longName).longOpt(longName).desc(description)
+        .numberOfArgs(0).build();
+  }
+
+  protected static Option makeOpt(String single, String longName, String description,
+      boolean required, int argc, Class<?> type) {
+    return Option.builder(single).argName(longName).required(required).longOpt(longName)
+        .desc(description).numberOfArgs(argc).type(type).build();
+  }
+
+  protected static Options buildCmdLineOptions() {
+    Options ret = new Options();
+
+    ret.addOption(
+        makeOpt("c", "config", "ElasticSearch configuration file", true, 1, String.class));
+
+    // MODE
+    OptionGroup group = new OptionGroup();
+    group.setRequired(true);
+    group.addOption(makeOpt("l", "last-run-file", "last run date file (yyyy-MM-dd HH:mm:ss)", false,
+        1, String.class));
+    group.addOption(makeOpt("X", "initial-load", "initial load"));
+    ret.addOptionGroup(group);
+
+    return ret;
+  }
+
+  protected static void printUsage() {
+    new HelpFormatter().printHelp("Client batch loader", buildCmdLineOptions());
+    System.exit(-1);
+  }
+
+  public static JobOptions parseCommandLine(String[] args) {
+    JobOptions ret = new JobOptions();
+    try {
+      Options options = buildCmdLineOptions();
+      CommandLineParser parser = new DefaultParser();
+      CommandLine cmd = parser.parse(options, args);
+
+      for (Option opt : cmd.getOptions()) {
+        switch (opt.getArgName()) {
+          case "config":
+            LOGGER.info("ES config file  = " + opt.getValue());
+            ret.esConfigLoc = opt.getValue().trim();
+            break;
+
+          case "last-run-file":
+            ret.lastRunMode = true;
+            ret.lastRunLoc = opt.getValue().trim();
+            LOGGER.info("last run file = " + ret.lastRunLoc);
+            break;
+
+          case "initial-load":
+            LOGGER.info("INITIAL LOAD!");
+            ret.lastRunMode = false;
+            break;
+
+          default:
+            break;
+        }
+      }
+    } catch (ParseException e) {
+      printUsage();
     }
 
-    final Injector injector =
-        Guice.createInjector(new JobsGuiceInjector(new File(args[0]), args[1]));
-    final ClientIndexerJob job = injector.getInstance(ClientIndexerJob.class);
-
-    // Let session factory and ElasticSearch dao close themselves automatically.
-    try (SessionFactory sessionFactory = job.sessionFactory;
-        ElasticsearchDao esDao = job.elasticsearchDao) {
-      job.run();
-    } catch (JobsException e) {
-      LOGGER.error("Unable to complete job", e);
-    }
+    return ret;
   }
 
   /**
@@ -99,13 +163,12 @@ public class ClientIndexerJob extends JobBasedOnLastSuccessfulRunTime {
           BulkProcessor.builder(elasticsearchDao.getClient(), new BulkProcessor.Listener() {
             @Override
             public void beforeBulk(long executionId, BulkRequest request) {
-              LOGGER.info("Going to execute new bulk composed of {} actions",
-                  request.numberOfActions());
+              LOGGER.info("Ready to execute bulk of {} actions", request.numberOfActions());
             }
 
             @Override
             public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
-              LOGGER.info("Executed bulk composed of {} actions", request.numberOfActions());
+              LOGGER.info("Executed bulk of {} actions", request.numberOfActions());
             }
 
             @Override
@@ -131,8 +194,7 @@ public class ClientIndexerJob extends JobBasedOnLastSuccessfulRunTime {
       }
 
       // Give it time to finish the last batch.
-      bulkProcessor.awaitClose(10, TimeUnit.SECONDS);
-
+      bulkProcessor.awaitClose(20, TimeUnit.SECONDS);
       LOGGER.info(MessageFormat.format("Indexed {0} people", results.size()));
       LOGGER.info(MessageFormat.format("Updating last succesful run time to {0}",
           jobDateFormat.format(startTime)));
@@ -147,9 +209,39 @@ public class ClientIndexerJob extends JobBasedOnLastSuccessfulRunTime {
     }
   }
 
+  /**
+   * Indexes a single document. Prefer batch mode.
+   * 
+   * @param person {@link Person} document to index
+   * @throws JsonProcessingException if JSON cannot be read
+   */
   private void indexDocument(Person person) throws JsonProcessingException {
     final String document = mapper.writeValueAsString(person);
     elasticsearchDao.index(document, person.getId().toString());
   }
+
+  /**
+   * Program entry point.
+   * 
+   * @param args command line arguments
+   */
+  public static void main(String... args) {
+
+    final JobOptions opts = parseCommandLine(args);
+    final Injector injector =
+        Guice.createInjector(new JobsGuiceInjector(new File(opts.esConfigLoc), opts.lastRunLoc));
+    final ClientIndexerJob job = injector.getInstance(ClientIndexerJob.class);
+
+    // Let session factory and ElasticSearch dao close themselves automatically.
+    try (SessionFactory sessionFactory = job.sessionFactory;
+        ElasticsearchDao esDao = job.elasticsearchDao) {
+      job.run();
+    } catch (JobsException e) {
+      LOGGER.error("Unable to complete job: {}", e.getMessage(), e);
+    } catch (IOException e) {
+      LOGGER.error("Unable to close resource: {}", e.getMessage(), e);
+    }
+  }
+
 }
 
