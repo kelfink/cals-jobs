14e2693bcddb8fe96bf2d761bcb33ddd5286c3ab
David R. Smith <drs.code.monkey@gmail.com>
2017-06-30 10:43:11 -0700

    Neutron JUnit

--- src/test/java/gov/ca/cwds/jobs/util/AsyncReadWriteJobTest.java
+++ src/test/java/gov/ca/cwds/jobs/util/AsyncReadWriteJobTest.java
@@ -8,14 +8,33 @@ import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import gov.ca.cwds.data.persistence.PersistentObject;
 import gov.ca.cwds.jobs.exception.JobsException;
 
 public class AsyncReadWriteJobTest {
+
+  public static class Input implements PersistentObject {
+    private Integer id;
+
+    public Input(Integer id) {
+      this.id = id;
+    }
+
+    @Override
+    public Integer getPrimaryKey() {
+      return id;
+    }
+  }
 
   @Mock
   JobReader<Object> reader;
@@ -26,12 +45,59 @@ public class AsyncReadWriteJobTest {
   @Mock
   JobWriter<Object> writer;
 
+  private List<Integer> input = new ArrayList<>();
+  private List<String> output = new ArrayList<>();
+
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
     reader = mock(JobReader.class);
     processor = mock(JobProcessor.class);
     writer = mock(JobWriter.class);
+
+    input.add(0);
+    input.add(1);
+    input.add(2);
+  }
+
+  @After
+  public void after() {
+    input.clear();
+    output.clear();
+  }
+
+  @Test
+  public void genericTest() {
+    AsyncReadWriteJob job = new AsyncReadWriteJob(() -> {
+      if (!input.isEmpty()) {
+        return input.remove(0);
+      }
+      return null;
+    }, String::valueOf, output::addAll);
+    job.run();
+
+    Assert.assertEquals(3, output.size());
+    for (int i = 0; i < output.size(); i++) {
+      Assert.assertEquals(String.valueOf(i), output.get(i));
+    }
+  }
+
+  @Test
+  public void testReaderException() {
+    AsyncReadWriteJob job = new AsyncReadWriteJob(() -> {
+      if (input.size() == 3) {
+        return input.remove(0);
+      } else {
+        Thread.sleep(1000); // NOSONAR
+        throw new RuntimeException("failed on second!");
+      }
+    }, String::valueOf, output::addAll);
+
+    job.run();
+    Assert.assertEquals(1, output.size());
+    for (int i = 0; i < output.size(); i++) {
+      Assert.assertEquals(String.valueOf(i), output.get(i));
+    }
   }
 
   @Test
@@ -40,8 +106,14 @@ public class AsyncReadWriteJobTest {
   }
 
   @Test
-  public void instantiation() throws Exception {
+  public void instantiate_1() throws Exception {
     AsyncReadWriteJob target = new AsyncReadWriteJob(reader, processor, writer);
+    assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void instantiate_2() throws Exception {
+    AsyncReadWriteJob target = new AsyncReadWriteJob(reader, writer);
     assertThat(target, notNullValue());
   }
 
