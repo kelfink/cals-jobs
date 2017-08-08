fd63cd66ad21ee699207efe76baf12359d2595e5
Shahid Saleemi <shahid.saleemi@osi.ca.gov>
2017-08-08 12:06:38 -0700

    Enable parallelism with db2

--- src/main/java/gov/ca/cwds/jobs/BasePersonIndexerJob.java
+++ src/main/java/gov/ca/cwds/jobs/BasePersonIndexerJob.java
@@ -644,7 +644,7 @@ public abstract class BasePersonIndexerJob<T extends PersistentObject, M extends
    */
   protected void prepareInsertCollections(ElasticSearchPerson esp, T t, String elementName,
       List<? extends ApiTypedIdentifier<String>> list, ESOptionalCollection... keep)
-      throws JsonProcessingException {
+          throws JsonProcessingException {
 
     // Null out optional collections for updates.
     esp.clearOptionalCollections(keep);
@@ -667,7 +667,7 @@ public abstract class BasePersonIndexerJob<T extends PersistentObject, M extends
    */
   protected Pair<String, String> prepareUpsertJson(ElasticSearchPerson esp, T t, String elementName,
       List<? extends ApiTypedIdentifier<String>> list, ESOptionalCollection... keep)
-      throws JsonProcessingException {
+          throws JsonProcessingException {
 
     // Child classes: Set optional collections before serializing the insert JSON.
     prepareInsertCollections(esp, t, elementName, list, keep);
@@ -882,7 +882,10 @@ public abstract class BasePersonIndexerJob<T extends PersistentObject, M extends
       final String query = getInitialLoadQuery(getDBSchemaName());
       M m;
 
-      con.nativeSQL("SET CURRENT DEGREE = 'ANY'");
+      /**
+       * Enable parallelism for underlying database
+       */
+      enableParallelism(con);
 
       try (Statement stmt = con.createStatement()) {
         stmt.setFetchSize(5000); // faster
@@ -1468,9 +1471,8 @@ public abstract class BasePersonIndexerJob<T extends PersistentObject, M extends
           : opts.getTotalBuckets();
       final javax.persistence.Query q = jobDao.getSessionFactory().createEntityManager()
           .createNativeQuery(QUERY_BUCKET_LIST.replaceAll("THE_TABLE", table)
-              .replaceAll("THE_ID_COL", getIdColumn()).replaceAll("THE_TOTAL_BUCKETS",
-                  String.valueOf(totalBuckets)),
-              BatchBucket.class);
+              .replaceAll("THE_ID_COL", getIdColumn())
+              .replaceAll("THE_TOTAL_BUCKETS", String.valueOf(totalBuckets)), BatchBucket.class);
 
       ret = q.getResultList();
       session.clear();
@@ -1671,5 +1673,12 @@ public abstract class BasePersonIndexerJob<T extends PersistentObject, M extends
    */
   protected static String getDBSchemaName() {
     return System.getProperty("DB_CMS_SCHEMA");
+  }
+
+  private static void enableParallelism(Connection con) throws SQLException {
+    String dbProductName = con.getMetaData().getDatabaseProductName();
+    if (StringUtils.containsIgnoreCase(dbProductName, "db2")) {
+      con.nativeSQL("SET CURRENT DEGREE = 'ANY'");
+    }
   }
 }
