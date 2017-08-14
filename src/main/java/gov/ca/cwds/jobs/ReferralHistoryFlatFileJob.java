0da1ace9b77566d6cfaa07e35d3e746bb37769e8
David R. Smith <drs.code.monkey@gmail.com>
2017-08-14 12:16:53 -0700

    #149768793: tune Referrals MQT

--- src/main/java/gov/ca/cwds/jobs/ReferralHistoryFlatFileJob.java
+++ src/main/java/gov/ca/cwds/jobs/ReferralHistoryFlatFileJob.java
@@ -3,6 +3,9 @@ package gov.ca.cwds.jobs;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import org.hibernate.SessionFactory;
@@ -17,6 +20,7 @@ import gov.ca.cwds.data.es.ElasticsearchDao;
 import gov.ca.cwds.data.persistence.cms.EsPersonReferral;
 import gov.ca.cwds.data.persistence.cms.ReplicatedPersonReferrals;
 import gov.ca.cwds.inject.CmsSessionFactory;
+import gov.ca.cwds.jobs.exception.JobsException;
 import gov.ca.cwds.jobs.inject.AltInputFile;
 import gov.ca.cwds.jobs.inject.LastRunFile;
 import gov.ca.cwds.jobs.util.JobLogUtils;
@@ -30,7 +34,9 @@ public class ReferralHistoryFlatFileJob extends ReferralHistoryIndexerJob {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ReferralHistoryFlatFileJob.class);
 
-  private String altInputFilename;
+  private String[] altInputFilenames;
+
+  private AtomicInteger nextThreadNum = new AtomicInteger(0);
 
   /**
    * Construct batch job instance with all required dependencies.
@@ -47,7 +53,7 @@ public class ReferralHistoryFlatFileJob extends ReferralHistoryIndexerJob {
       @LastRunFile String lastJobRunTimeFilename, ObjectMapper mapper,
       @CmsSessionFactory SessionFactory sessionFactory, @AltInputFile String altInputFilename) {
     super(clientDao, esDao, lastJobRunTimeFilename, mapper, sessionFactory);
-    this.altInputFilename = altInputFilename;
+    this.altInputFilenames = altInputFilename.split(",");
   }
 
   /**
@@ -55,8 +61,13 @@ public class ReferralHistoryFlatFileJob extends ReferralHistoryIndexerJob {
    * 
    * @param r referral to hand to next queue
    */
-  protected void handOff(final EsPersonReferral r) {
+  protected void handOff(String line) {
     try {
+      if (fatalError) {
+        throw new JobsException("Previous failure in other thread!");
+      }
+
+      final EsPersonReferral r = EsPersonReferral.parseLine(line);
       if (r != null) {
         queueTransform.putLast(r);
       }
@@ -66,25 +77,51 @@ public class ReferralHistoryFlatFileJob extends ReferralHistoryIndexerJob {
     }
   }
 
+  /**
+   * Launch an extract thread for a single file. Maintain file order by client id and referral id.
+   */
+  protected void startExtractThread() {
+    final int i = nextThreadNum.getAndIncrement();
+    Thread.currentThread().setName("extract_" + i);
+    LOGGER.info("BEGIN: flat file extract " + i);
+
+    final Path pathIn = Paths.get(altInputFilenames[i]);
+    try (Stream<String> lines = Files.lines(pathIn)) {
+      lines.sequential().forEach(this::handOff);
+    } catch (Exception e) {
+      fatalError = true;
+      JobLogUtils.raiseError(LOGGER, e, "BATCH ERROR! {}", e.getMessage());
+    }
+
+    LOGGER.info("DONE: flat file extract " + i);
+  }
+
   @Override
   protected void threadExtractJdbc() {
     Thread.currentThread().setName("extract");
     LOGGER.info("BEGIN: flat file extract");
 
-    Path pathIn = Paths.get(altInputFilename);
-    try (Stream<String> lines = Files.lines(pathIn)) {
+    final Deque<Thread> threads = new ArrayDeque<>();
+    for (int i = 0; i < this.altInputFilenames.length; i++) {
+      Thread t = new Thread(this::startExtractThread);
+      threads.push(t);
+      t.start();
+    }
 
-      // Maintain file order by client, referral.
-      lines.sequential().map(EsPersonReferral::parseLine).forEach(this::handOff);
-
-    } catch (Exception e) {
+    Thread t;
+    try {
+      while ((t = threads.pop()) != null) {
+        t.join();
+      }
+    } catch (InterruptedException ie) { // NOSONAR
+      LOGGER.warn("interrupted: {}", ie.getMessage(), ie);
       fatalError = true;
-      JobLogUtils.raiseError(LOGGER, e, "BATCH ERROR! {}", e.getMessage());
+      Thread.currentThread().interrupt();
     } finally {
       doneExtract = true;
     }
 
-    LOGGER.info("DONE: Stage #1: Extract");
+    LOGGER.info("DONE: flat file Extract");
   }
 
   /**
