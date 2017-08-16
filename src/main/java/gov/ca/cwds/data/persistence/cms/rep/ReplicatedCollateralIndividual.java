7873e78c3555cb4f2be33b874ec411d36d8911b1
David R. Smith <drs.code.monkey@gmail.com>
2017-08-15 19:53:30 -0700

    #149768793: tune Referrals MQT

--- src/main/java/gov/ca/cwds/data/persistence/cms/rep/ReplicatedCollateralIndividual.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/rep/ReplicatedCollateralIndividual.java
@@ -30,34 +30,23 @@ import gov.ca.cwds.rest.api.domain.cms.LegacyTable;
  */
 @NamedNativeQueries({
     @NamedNativeQuery(
-        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedCollateralIndividual.findBucketRange",
-        query = "SELECT x.* FROM {h-schema}COLTRL_T x "
-            + "WHERE x.IDENTIFIER BETWEEN :min_id AND :max_id ORDER BY IDENTIFIER FOR READ ONLY WITH UR",
-        resultClass = ReplicatedCollateralIndividual.class, readOnly = true),
-    @NamedNativeQuery(
-        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedCollateralIndividual.findAllUpdatedAfter",
-        query = "select z.IDENTIFIER, z.BADGE_NO, z.CITY_NM, z.EMPLYR_NM, z.FAX_NO, "
-            + "z.FIRST_NM, z.FRG_ADRT_B, z.LAST_NM, z.MID_INI_NM, z.NMPRFX_DSC, "
-            + "z.PRM_TEL_NO, z.PRM_EXT_NO, z.STATE_C, z.STREET_NM, z.STREET_NO, "
-            + "z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, z.LST_UPD_TS, z.ZIP_SFX_NO, "
-            + "z.COMNT_DSC, z.GENDER_CD, z.BIRTH_DT, z.MRTL_STC, z.EMAIL_ADDR, "
-            + "z.ESTBLSH_CD, z.ESTBLSH_ID, z.RESOST_IND "
-            + ", z.IBMSNAP_OPERATION, z.IBMSNAP_LOGMARKER "
-            + "from {h-schema}COLTRL_T z WHERE z.IBMSNAP_LOGMARKER >= :after FOR READ ONLY WITH UR ",
+        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedCollateralIndividualR1.findAllUpdatedAfter",
+        query = "select z.IDENTIFIER, z.BADGE_NO, z.CITY_NM, z.EMPLYR_NM, z.FAX_NO, z.FIRST_NM, z.FRG_ADRT_B, "
+            + "z.LAST_NM, z.MID_INI_NM, z.NMPRFX_DSC, z.PRM_TEL_NO, z.PRM_EXT_NO, z.STATE_C, z.STREET_NM, "
+            + "z.STREET_NO, z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, z.LST_UPD_TS, z.ZIP_SFX_NO, z.COMNT_DSC, "
+            + "z.GENDER_CD, z.BIRTH_DT, z.MRTL_STC, z.EMAIL_ADDR, z.ESTBLSH_CD, z.ESTBLSH_ID, z.RESOST_IND, "
+            + "IBMSNAP_OPERATION, z.IBMSNAP_LOGMARKER FROM {h-schema}COLTRL_T z "
+            + "WHERE z.IBMSNAP_LOGMARKER >= :after FOR READ ONLY WITH UR",
         resultClass = ReplicatedCollateralIndividual.class),
     @NamedNativeQuery(
-        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedCollateralIndividual.findPartitionedBuckets",
-        query = "select z.IDENTIFIER, z.BADGE_NO, z.CITY_NM, z.EMPLYR_NM, z.FAX_NO, "
-            + "z.FIRST_NM, z.FRG_ADRT_B, z.LAST_NM, z.MID_INI_NM, z.NMPRFX_DSC, "
-            + "z.PRM_TEL_NO, z.PRM_EXT_NO, z.STATE_C, z.STREET_NM, z.STREET_NO, "
-            + "z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, z.LST_UPD_TS, z.ZIP_SFX_NO, "
-            + "z.COMNT_DSC, z.GENDER_CD, z.BIRTH_DT, z.MRTL_STC, z.EMAIL_ADDR, "
-            + "z.ESTBLSH_CD, z.ESTBLSH_ID, z.RESOST_IND "
-            + ", 'U' as IBMSNAP_OPERATION, z.LST_UPD_TS as IBMSNAP_LOGMARKER "
-            + "from ( select mod(y.rn, CAST(:total_buckets AS INTEGER)) + 1 as bucket, y.* "
-            + "from ( select row_number() over (order by 1) as rn, x.* "
-            + "from {h-schema}COLTRL_T x WHERE x.IDENTIFIER >= :min_id and x.IDENTIFIER < :max_id "
-            + ") y ) z where z.bucket = :bucket_num FOR READ ONLY WITH UR",
+        name = "gov.ca.cwds.data.persistence.cms.rep.ReplicatedCollateralIndividualR1.findPartitionedBuckets",
+        query = "select z.IDENTIFIER, z.BADGE_NO, z.CITY_NM, z.EMPLYR_NM, z.FAX_NO, z.FIRST_NM, z.FRG_ADRT_B, "
+            + "z.LAST_NM, z.MID_INI_NM, z.NMPRFX_DSC, z.PRM_TEL_NO, z.PRM_EXT_NO, z.STATE_C, z.STREET_NM, "
+            + "z.STREET_NO, z.SUFX_TLDSC, z.ZIP_NO, z.LST_UPD_ID, z.LST_UPD_TS, z.ZIP_SFX_NO, z.COMNT_DSC, "
+            + "z.GENDER_CD, z.BIRTH_DT, z.MRTL_STC, z.EMAIL_ADDR, z.ESTBLSH_CD, z.ESTBLSH_ID, z.RESOST_IND, "
+            + "IBMSNAP_OPERATION, z.IBMSNAP_LOGMARKER " + "FROM {h-schema}COLTRL_T z "
+            + "WHERE z.IDENTIFIER >= :min_id AND z.IDENTIFIER < :max_id "
+            + "AND (1=1 OR 57 = :bucket_num OR 92 = :total_buckets) FOR READ ONLY WITH UR",
         resultClass = ReplicatedCollateralIndividual.class)})
 @Entity
 @Table(name = "COLTRL_T")
