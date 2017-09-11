cc5cccc2bae86664059a0d3072dd78ba2b2cf62d
David R. Smith <drs.code.monkey@gmail.com>
2017-09-11 16:20:47 -0700

    #150520695: LOV

--- db/ddl/postgres/VW_INTAKE_LOV.sql
+++ db/ddl/postgres/VW_INTAKE_LOV.sql
@@ -19,8 +19,8 @@ CREATE VIEW VW_INTAKE_LOV (
 	SELECT 
 	ct.lg_meta, 
 	ct.intake_type, 
-	ct.PARENT_CAT_ID, 
-	ct2.INTAKE_TYPE as PARENT_INTAKE_TYPE, 
+	par_ct.cat_id as PARENT_CAT_ID, 
+	par_ct.INTAKE_TYPE as PARENT_INTAKE_TYPE, 
 	cd.lg_sys_id, 
 	cd.parent_lg_sys_id, 
 	sc.logical_id as LG_LOG_ID,
@@ -32,10 +32,10 @@ CREATE VIEW VW_INTAKE_LOV (
 	cd.intake_code,
 	cd.intake_display
 	FROM intake_lov_category      ct
-	JOIN intake_lov_code          cd  ON ct.cat_id            = cd.cat_id
-	JOIN system_codes             sc  ON sc."id"              = cd.lg_sys_id
-	LEFT JOIN intake_lov_code     cd2 ON cd2.parent_lg_sys_id = cd.lg_sys_id
-	LEFT JOIN intake_lov_category ct2 ON ct2.cat_id           = cd2.cat_id
+	JOIN intake_lov_code          cd     ON ct.cat_id         = cd.cat_id
+	JOIN system_codes             sc     ON sc."id"           = cd.lg_sys_id
+	LEFT JOIN intake_lov_code     par_cd ON par_cd.lg_sys_id  = cd.parent_lg_sys_id
+	LEFT JOIN intake_lov_category par_ct ON par_ct.cat_id     = ct.parent_cat_id
 ;
 
 
