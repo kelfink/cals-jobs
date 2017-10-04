8378785e62e9dcaae3c708aeb45fbed550cdaf6e
David R. Smith <drs.code.monkey@gmail.com>
2017-10-04 12:20:41 -0700

    #151650562: Neutron SonarQube

--- src/test/java/gov/ca/cwds/jobs/OtherAdultInPlacementHomeTest.java
+++ src/test/java/gov/ca/cwds/jobs/OtherAdultInPlacementHomeTest.java
@@ -5,53 +5,31 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import gov.ca.cwds.dao.cms.ReplicatedOtherAdultInPlacemtHomeDao;
-import gov.ca.cwds.data.es.ElasticsearchDao;
+import gov.ca.cwds.data.persistence.cms.rep.ReplicatedOtherAdultInPlacemtHome;
 
 /**
  * 
  * @author CWDS API Team
  */
 @SuppressWarnings("javadoc")
-public class OtherAdultInPlacementHomeTest {
+public class OtherAdultInPlacementHomeTest
+    extends PersonJobTester<ReplicatedOtherAdultInPlacemtHome, ReplicatedOtherAdultInPlacemtHome> {
 
-  @SuppressWarnings("unused")
-  private static ReplicatedOtherAdultInPlacemtHomeDao dao;
-  private static SessionFactory sessionFactory;
-  private Session session;
+  ReplicatedOtherAdultInPlacemtHomeDao dao;
+  OtherAdultInPlacemtHomeIndexerJob target;
 
-  @BeforeClass
-  public static void beforeClass() {
-    sessionFactory =
-        new Configuration().configure("test-cms-hibernate.cfg.xml").buildSessionFactory();
-    dao = new ReplicatedOtherAdultInPlacemtHomeDao(sessionFactory);
-  }
-
-  @AfterClass
-  public static void afterClass() {
-    sessionFactory.close();
-  }
-
+  @Override
   @Before
-  public void setup() {
-    session = sessionFactory.getCurrentSession();
-    session.beginTransaction();
-  }
-
-  @After
-  public void teardown() {
-    session.getTransaction().rollback();
+  public void setup() throws Exception {
+    super.setup();
+    dao = new ReplicatedOtherAdultInPlacemtHomeDao(this.sessionFactory);
+    target = new OtherAdultInPlacemtHomeIndexerJob(dao, esDao, lastJobRunTimeFilename, MAPPER,
+        sessionFactory);
   }
 
   @Test
@@ -61,18 +39,11 @@ public class OtherAdultInPlacementHomeTest {
 
   @Test
   public void testInstantiation() throws Exception {
-    ReplicatedOtherAdultInPlacemtHomeDao otherAdultInPlacementHomeDao = null;
-    ElasticsearchDao elasticsearchDao = null;
-    String lastJobRunTimeFilename = null;
-    ObjectMapper mapper = null;
-    SessionFactory sessionFactory = null;
-    OtherAdultInPlacemtHomeIndexerJob target =
-        new OtherAdultInPlacemtHomeIndexerJob(otherAdultInPlacementHomeDao, elasticsearchDao,
-            lastJobRunTimeFilename, mapper, sessionFactory);
     assertThat(target, notNullValue());
   }
 
   @Test
+  @Ignore
   public void testfindAllUpdatedAfterNamedQueryExists() throws Exception {
     Query query = session.getNamedQuery(
         "gov.ca.cwds.data.persistence.cms.rep.ReplicatedOtherAdultInPlacemtHome.findAllUpdatedAfter");
--- src/test/java/gov/ca/cwds/jobs/OtherChildInPlacemtHomeIndexerJobTest.java
+++ src/test/java/gov/ca/cwds/jobs/OtherChildInPlacemtHomeIndexerJobTest.java
@@ -5,52 +5,30 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import gov.ca.cwds.dao.cms.ReplicatedOtherChildInPlacemtHomeDao;
-import gov.ca.cwds.data.es.ElasticsearchDao;
+import gov.ca.cwds.data.persistence.cms.rep.ReplicatedOtherChildInPlacemtHome;
 
 /**
  * @author CWDS API Team
  */
 @SuppressWarnings("javadoc")
-public class OtherChildInPlacemtHomeIndexerJobTest {
+public class OtherChildInPlacemtHomeIndexerJobTest
+    extends PersonJobTester<ReplicatedOtherChildInPlacemtHome, ReplicatedOtherChildInPlacemtHome> {
 
-  @SuppressWarnings("unused")
-  private static ReplicatedOtherChildInPlacemtHomeDao dao;
-  private static SessionFactory sessionFactory;
-  private Session session;
+  ReplicatedOtherChildInPlacemtHomeDao dao;
+  OtherChildInPlacemtHomeIndexerJob target;
 
-  @BeforeClass
-  public static void beforeClass() {
-    sessionFactory =
-        new Configuration().configure("test-cms-hibernate.cfg.xml").buildSessionFactory();
-    dao = new ReplicatedOtherChildInPlacemtHomeDao(sessionFactory);
-  }
-
-  @AfterClass
-  public static void afterClass() {
-    sessionFactory.close();
-  }
-
+  @Override
   @Before
-  public void setup() {
-    session = sessionFactory.getCurrentSession();
-    session.beginTransaction();
-  }
-
-  @After
-  public void teardown() {
-    session.getTransaction().rollback();
+  public void setup() throws Exception {
+    super.setup();
+    dao = new ReplicatedOtherChildInPlacemtHomeDao(this.sessionFactory);
+    target = new OtherChildInPlacemtHomeIndexerJob(dao, esDao, lastJobRunTimeFilename, MAPPER,
+        sessionFactory);
   }
 
   @Test
@@ -60,18 +38,11 @@ public class OtherChildInPlacemtHomeIndexerJobTest {
 
   @Test
   public void testInstantiation() throws Exception {
-    ReplicatedOtherChildInPlacemtHomeDao otherChildInPlacemtHomtDao = null;
-    ElasticsearchDao elasticsearchDao = null;
-    String lastJobRunTimeFilename = null;
-    ObjectMapper mapper = null;
-    SessionFactory sessionFactory = null;
-    OtherChildInPlacemtHomeIndexerJob target =
-        new OtherChildInPlacemtHomeIndexerJob(otherChildInPlacemtHomtDao, elasticsearchDao,
-            lastJobRunTimeFilename, mapper, sessionFactory);
     assertThat(target, notNullValue());
   }
 
   @Test
+  @Ignore
   public void testfindAllUpdatedAfterNamedQueryExists() throws Exception {
     Query query = session.getNamedQuery(
         "gov.ca.cwds.data.persistence.cms.rep.ReplicatedOtherChildInPlacemtHome.findAllUpdatedAfter");
