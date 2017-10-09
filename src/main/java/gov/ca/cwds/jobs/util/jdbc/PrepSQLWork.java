da372931911def5077ea495aaf5379944e3660cc
David R. Smith <David.Smith@osi.ca.gov>
2017-10-09 13:38:10 -0700

    #151650562: Neutron SonarQube

--- src/main/java/gov/ca/cwds/jobs/util/jdbc/PrepSQLWork.java
+++ src/main/java/gov/ca/cwds/jobs/util/jdbc/PrepSQLWork.java
@@ -1,20 +1,31 @@
 package gov.ca.cwds.jobs.util.jdbc;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Date;
 
 import org.hibernate.jdbc.Work;
 
+/**
+ * Execute SQL prior to retrieving records, typically for last change runs. Examples include
+ * populating a global temporary table prior to reading from a view.
+ * 
+ * @author CWDS API Team
+ */
 public class PrepSQLWork implements Work {
 
   private final Date lastRunTime;
   private final String sqlInsertLastChange;
 
+  /**
+   * Constructor.
+   * 
+   * @param lastRunTime last successful run time
+   * @param sqlInsertLastChange SQL to run
+   */
   public PrepSQLWork(Date lastRunTime, String sqlInsertLastChange) {
-    this.lastRunTime = lastRunTime;
+    this.lastRunTime = lastRunTime != null ? new Date(lastRunTime.getTime()) : null;
     this.sqlInsertLastChange = sqlInsertLastChange;
   }
 
@@ -31,10 +42,9 @@ public class PrepSQLWork implements Work {
         .replaceAll("##TIMESTAMP##", buf.toString());
     JobJdbcUtils.LOGGER.info("Prep SQL: {}", sql);
 
-    try (final Statement stmt =
-        con.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)) {
+    try (final PreparedStatement stmt = con.prepareStatement(sql)) {
       JobJdbcUtils.LOGGER.info("Find keys new/changed since {}", lastRunTime);
-      final int cntNewChanged = stmt.executeUpdate(sql);
+      final int cntNewChanged = stmt.executeUpdate();
       JobJdbcUtils.LOGGER.info("Total keys new/changed: {}", cntNewChanged);
     }
   }
--- src/test/java/gov/ca/cwds/jobs/util/jdbc/PrepSQLWorkTest.java
+++ src/test/java/gov/ca/cwds/jobs/util/jdbc/PrepSQLWorkTest.java
@@ -4,8 +4,10 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
 
@@ -18,12 +20,17 @@ import gov.ca.cwds.jobs.PersonJobTester;
 
 public class PrepSQLWorkTest extends PersonJobTester<ReplicatedPersonCases, EsPersonCase> {
 
+  protected PreparedStatement prepStmt;
   PrepSQLWork target;
 
   @Override
   @Before
   public void setup() throws Exception {
     super.setup();
+
+    prepStmt = mock(PreparedStatement.class);
+    when(con.prepareStatement(any(String.class))).thenReturn(prepStmt);
+    when(prepStmt.executeUpdate()).thenReturn(10);
   }
 
   @Test
@@ -45,7 +52,6 @@ public class PrepSQLWorkTest extends PersonJobTester<ReplicatedPersonCases, EsPe
     final Date lastRunTime = new Date();
     final String sqlInsertLastChange =
         "SELECT C.* FROM CLIENT_T C WHERE c.LST_UPD_TS > '2017-08-28 11:54:40'";
-    when(con.createStatement(any(Integer.class), any(Integer.class))).thenReturn(stmt);
 
     target = new PrepSQLWork(lastRunTime, sqlInsertLastChange);
     target.execute(con);
@@ -56,7 +62,8 @@ public class PrepSQLWorkTest extends PersonJobTester<ReplicatedPersonCases, EsPe
     final Date lastRunTime = new Date();
     final String sqlInsertLastChange =
         "SELECT C.* FROM CLIENT_T C WHERE c.LST_UPD_TS > '2017-08-28 11:54:40'";
-    when(con.createStatement(any(Integer.class), any(Integer.class))).thenThrow(SQLException.class);
+
+    when(prepStmt.executeUpdate()).thenThrow(SQLException.class);
 
     target = new PrepSQLWork(lastRunTime, sqlInsertLastChange);
     target.execute(con);
