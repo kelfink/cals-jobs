6125d50c85d35f74fc56905c64299a8b340f12c4
David R. Smith <David.Smith@osi.ca.gov>
2017-10-24 15:09:47 -0700

    #152121892: automate ES validation

--- src/main/java/gov/ca/cwds/jobs/service/NeutronElasticValidator.java
+++ src/main/java/gov/ca/cwds/jobs/service/NeutronElasticValidator.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 
 import gov.ca.cwds.dao.cms.ReplicatedClientAddressDao;
 import gov.ca.cwds.dao.cms.ReplicatedClientDao;
+import gov.ca.cwds.dao.cms.ReplicatedClientRelationshipDao;
 import gov.ca.cwds.dao.cms.ReplicatedCollateralIndividualDao;
 import gov.ca.cwds.dao.cms.ReplicatedOtherClientNameDao;
 import gov.ca.cwds.dao.cms.ReplicatedReporterDao;
@@ -12,22 +13,28 @@ public class NeutronElasticValidator {
 
   private final ReplicatedClientDao repClientDao;
   private final ReplicatedClientAddressDao repClientAddressDao;
-  private final ReplicatedCollateralIndividualDao repCollateralIndividualDao;
 
   private final ReplicatedReporterDao repReporterDao;
+
+  private final ReplicatedCollateralIndividualDao repCollateralIndividualDao;
+
   private final ReplicatedOtherClientNameDao repOtherClientNameDao;
+
+  private final ReplicatedClientRelationshipDao repClientRelationshipDao;
 
   @Inject
   public NeutronElasticValidator(final ReplicatedClientDao repClientDao,
       final ReplicatedClientAddressDao repClientAddressDao,
       final ReplicatedCollateralIndividualDao repCollateralIndividualDao,
       final ReplicatedReporterDao repReporterDao,
-      final ReplicatedOtherClientNameDao repOtherClientNameDao) {
+      final ReplicatedOtherClientNameDao repOtherClientNameDao,
+      final ReplicatedClientRelationshipDao repClientRelationshipDao) {
     this.repClientDao = repClientDao;
     this.repClientAddressDao = repClientAddressDao;
     this.repCollateralIndividualDao = repCollateralIndividualDao;
     this.repReporterDao = repReporterDao;
     this.repOtherClientNameDao = repOtherClientNameDao;
+    this.repClientRelationshipDao = repClientRelationshipDao;
   }
 
 }
