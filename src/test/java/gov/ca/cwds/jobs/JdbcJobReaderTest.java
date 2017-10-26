734d6e7231f07822f2010d03833491c9847de697
David R. Smith <David.Smith@osi.ca.gov>
2017-10-26 09:34:02 -0700

    #152121892: SonarQube vulnerability

--- src/test/java/gov/ca/cwds/jobs/JdbcJobReaderTest.java
+++ src/test/java/gov/ca/cwds/jobs/JdbcJobReaderTest.java
@@ -1,15 +1,28 @@
 package gov.ca.cwds.jobs;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.function.Function;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import gov.ca.cwds.data.persistence.PersistentObject;
+import gov.ca.cwds.jobs.config.JobConfiguration;
+import gov.ca.cwds.jobs.facility.FacilityRowMapper;
+import gov.ca.cwds.jobs.util.JobLogs;
 import gov.ca.cwds.jobs.util.JobReader;
 import gov.ca.cwds.jobs.util.jdbc.JdbcJobReader;
 import gov.ca.cwds.jobs.util.jdbc.RowMapper;
@@ -17,14 +30,49 @@ import gov.ca.cwds.jobs.util.jdbc.RowMapper;
 /**
  * @author CWDS Elasticsearch Team
  */
-public class JdbcJobReaderTest {
+public class JdbcJobReaderTest extends PersonJobTester {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(JdbcJobReaderTest.class);
+
+  File config;
+  JobConfiguration jobConfiguration;
+  FacilityRowMapper facilityRowMapper;
+  SessionFactory sessionFactory;
+  PreparedStatement statement;
+  Function<Connection, PreparedStatement> func;
+  JobConfiguration config_;
+
+  @Override
+  @Before
+  public void setup() throws Exception {
+    super.setup();
+
+    config_ = mock(JobConfiguration.class);
+    jobConfiguration = mock(JobConfiguration.class);
+    facilityRowMapper = mock(FacilityRowMapper.class);
+    sessionFactory = mock(SessionFactory.class);
+    statement = mock(PreparedStatement.class);
+    func = createPreparedStatementMaker();
+
+    when(statement.executeQuery()).thenReturn(rs);
+  }
+
+  public Function<Connection, PreparedStatement> createPreparedStatementMaker() {
+    return c -> {
+      try {
+        return c.prepareStatement("select * from test order by a");
+      } catch (SQLException e) {
+        throw JobLogs.buildRuntimeException(LOGGER, e, "FAILED TO PREPARE STATEMENT!",
+            e.getMessage());
+      }
+    };
+  }
 
   @Test
   public void testRead() throws Exception {
     SessionFactory sessionFactory =
         new Configuration().configure("test-lis-hibernate.cfg.xml").buildSessionFactory();
-    JobReader<Item> jobReader =
-        new JdbcJobReader<>(sessionFactory, new ItemMapper(), "select * from test order by a");
+    JobReader<Item> jobReader = new JdbcJobReader<>(sessionFactory, new ItemMapper(), func);
     jobReader.init();
     int counter = 0;
     Item item;
@@ -61,6 +109,7 @@ public class JdbcJobReaderTest {
     public Serializable getPrimaryKey() {
       return a;
     }
+
   }
 
   private static class ItemMapper implements RowMapper<Item> {
