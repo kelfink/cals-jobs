082dfa8cb79fe2dd2925da0eb87b1ed937401e50
David R. Smith <David.Smith@osi.ca.gov>
2017-10-30 19:26:53 -0700

    #152121892: code coverage

--- src/main/java/gov/ca/cwds/jobs/ClientIndexerJob.java
+++ src/main/java/gov/ca/cwds/jobs/ClientIndexerJob.java
@@ -188,12 +188,18 @@ public class ClientIndexerJob extends BasePersonIndexerJob<ReplicatedClient, EsC
     final Map<String, ElasticSearchPersonAddress> docAddresses = person.getAddresses().stream()
         .collect(Collectors.toMap(ElasticSearchPersonAddress::getId, a -> a));
 
-    // Not intersecting, though keys match.
-    // final SetView<Map.Entry<String, ReplicatedAddress>> intersection =
-    // Sets.intersection(repAddresses.entrySet(), docAddresses.entrySet());
-
     for (ElasticSearchPersonAddress docAddr : docAddresses.values()) {
+      if (!repAddresses.containsKey(docAddr.getAddressId())) {
+        LOGGER.warn("DOC ADDRESS ID {} NOT FOUND IN DATABASE {}", docAddr.getAddressId(), clientId);
+        return false;
+      }
+    }
 
+    for (ReplicatedAddress repAddr : repAddresses.values()) {
+      if (!docAddresses.containsKey(repAddr.getAddressId())) {
+        LOGGER.warn("ADDRESS ID {} NOT FOUND IN DOCUMENT {}", repAddr.getAddressId(), clientId);
+        return false;
+      }
     }
 
     LOGGER.warn("set size: docAddresses: {}, repAddresses: {}, client addrs: {}, doc addrs: {}",
