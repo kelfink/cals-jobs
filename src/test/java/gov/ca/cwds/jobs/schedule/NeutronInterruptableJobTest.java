f34c3b70b475d6961aa17205c7a05f24a813c183
David R. Smith <David.Smith@osi.ca.gov>
2017-11-01 13:07:43 -0700

    #152121892: decouple components

--- src/test/java/gov/ca/cwds/jobs/schedule/NeutronInterruptableJobTest.java
+++ src/test/java/gov/ca/cwds/jobs/schedule/NeutronInterruptableJobTest.java
@@ -30,6 +30,7 @@ public class NeutronInterruptableJobTest extends PersonJobTester {
   @Override
   @Before
   public void setup() throws Exception {
+    super.setup();
     dao = new TestNormalizedEntityDao(sessionFactory);
     rocket =
         new TestIndexerJob(dao, esDao, lastJobRunTimeFilename, MAPPER, sessionFactory, jobHistory);
