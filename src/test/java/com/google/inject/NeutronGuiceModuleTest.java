60e45944870c636bc961ebbe64225f7ebfe85754
David R. Smith <David.Smith@osi.ca.gov>
2017-11-04 00:19:25 -0700

    #152550170: Neutron: code coverage

---
+++ src/test/java/com/google/inject/NeutronGuiceModuleTest.java
@@ -0,0 +1,58 @@
+package com.google.inject;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+import gov.ca.cwds.jobs.PersonJobTester;
+
+public class NeutronGuiceModuleTest extends PersonJobTester {
+  public static class TestNeutronGuiceModule extends NeutronGuiceModule {
+
+    @Override
+    protected void configure() {}
+
+  }
+
+  NeutronGuiceModule target;
+
+  @Override
+  public void setup() throws Exception {
+    super.setup();
+    target = new TestNeutronGuiceModule();
+  }
+
+  @Test
+  public void type() throws Exception {
+    assertThat(NeutronGuiceModule.class, notNullValue());
+  }
+
+  @Test
+  public void instantiation() throws Exception {
+    assertThat(target, notNullValue());
+  }
+
+  @Test
+  public void binder_Args__() throws Exception {
+    Object actual = target.binder();
+    Object expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void getTestBinder_Args__() throws Exception {
+    Object actual = target.getTestBinder();
+    Object expected = null;
+    assertThat(actual, is(equalTo(expected)));
+  }
+
+  @Test
+  public void setTestBinder_Args__Object() throws Exception {
+    target.setTestBinder(mock(Binder.class));
+  }
+
+}
