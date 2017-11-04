59566c7b0904cff76e53c889b327edeeba41b5e4
David R. Smith <David.Smith@osi.ca.gov>
2017-11-04 00:39:35 -0700

    #152550170: Neutron: code coverage

--- src/test/java/gov/ca/cwds/jobs/BasePersonIndexerJobTest.java
+++ src/test/java/gov/ca/cwds/jobs/BasePersonIndexerJobTest.java
@@ -676,7 +676,7 @@ public class BasePersonIndexerJobTest
     assertThat(actual, notNullValue());
   }
 
-  @Test(expected = JobsException.class)
+  @Test(expected = SQLException.class)
   public void extractLastRunRecsFromView_Args__sql_error() throws Exception {
     final NativeQuery<TestDenormalizedEntity> qn = mock(NativeQuery.class);
     when(session.getNamedNativeQuery(any())).thenThrow(SQLException.class);
