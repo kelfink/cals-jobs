7cc3d199bef130f7f12c7ecc51abd67ffe1b2a46
David R. Smith <David.Smith@osi.ca.gov>
2017-11-07 15:15:27 -0800

    #151608667: refresh materialized query tables

--- src/test/java/gov/ca/cwds/neutron/jetpack/JobLogsTest.java
+++ src/test/java/gov/ca/cwds/neutron/jetpack/JobLogsTest.java
@@ -1,5 +1,6 @@
 package gov.ca.cwds.neutron.jetpack;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
@@ -9,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import gov.ca.cwds.jobs.exception.JobsException;
-import gov.ca.cwds.neutron.jetpack.JobLogs;
+import gov.ca.cwds.jobs.exception.NeutronException;
 
 public class JobLogsTest {
 
@@ -84,5 +85,74 @@ public class JobLogsTest {
     throw JobLogs.buildRuntimeException(log, e, "something bad", "who", "cares");
   }
 
-}
+  @Test
+  public void logEvery_Args__Logger__int__int__String__ObjectArray() throws Exception {
+    Logger log = mock(Logger.class);
+    int logEvery = 0;
+    int cntr = 0;
+    String action = null;
+    Object[] args = new Object[] {};
+    JobLogs.logEvery(log, logEvery, cntr, action, args);
+  }
 
+  @Test
+  public void logEvery_Args__Logger__int__String__ObjectArray() throws Exception {
+    Logger log = mock(Logger.class);
+    int cntr = 0;
+    String action = null;
+    Object[] args = new Object[] {};
+    for (int i = 0; i < 10000; i++) {
+      JobLogs.logEvery(log, ++cntr, action, args);
+    }
+  }
+
+  @Test
+  public void buildRuntimeException_Args__Logger__Throwable__String__ObjectArray()
+      throws Exception {
+    Logger log = mock(Logger.class);
+    Throwable e = null;
+    String pattern = null;
+    Object[] args = new Object[] {};
+    JobsException actual = JobLogs.buildRuntimeException(log, e, pattern, args);
+    assertThat(actual, is(notNullValue()));
+  }
+
+  @Test
+  public void buildCheckedException_Args__Logger__Throwable__String__ObjectArray()
+      throws Exception {
+    Logger log = mock(Logger.class);
+    Throwable e = null;
+    String pattern = null;
+    Object[] args = new Object[] {};
+    NeutronException actual = JobLogs.buildCheckedException(log, e, pattern, args);
+    assertThat(actual, is(notNullValue()));
+  }
+
+  @Test
+  public void checked_Args__Logger__Throwable__String__ObjectArray() throws Exception {
+    Logger log = mock(Logger.class);
+    Throwable e = null;
+    String pattern = null;
+    Object[] args = new Object[] {};
+    NeutronException actual = JobLogs.checked(log, e, pattern, args);
+    assertThat(actual, is(notNullValue()));
+  }
+
+  @Test
+  public void runtime_Args__Logger__Throwable__String__ObjectArray() throws Exception {
+    Logger log = mock(Logger.class);
+    Throwable e = null;
+    String pattern = null;
+    Object[] args = new Object[] {};
+    JobsException actual = JobLogs.runtime(log, e, pattern, args);
+    assertThat(actual, is(notNullValue()));
+  }
+
+  @Test
+  public void stackToString_Args__Exception() throws Exception {
+    Exception e = new IllegalStateException("test this");
+    String actual = JobLogs.stackToString(e);
+    assertThat(actual, is(notNullValue()));
+  }
+
+}
