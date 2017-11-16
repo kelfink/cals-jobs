5841f3d93aa106be9c012f298098bc3f8cce2493
David R. Smith <David.Smith@osi.ca.gov>
2017-11-15 17:28:55 -0800

    #152130766: Neutron continuous mode

--- src/test/java/gov/ca/cwds/jobs/util/elastic/ElasticJobWriterTest.java
+++ src/test/java/gov/ca/cwds/jobs/util/elastic/ElasticJobWriterTest.java
@@ -2,19 +2,25 @@ package gov.ca.cwds.jobs.util.elastic;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gov.ca.cwds.data.es.ElasticSearchPerson;
 import gov.ca.cwds.data.es.Elasticsearch5xDao;
 import gov.ca.cwds.jobs.Goddard;
+import gov.ca.cwds.jobs.test.TestNormalizedEntity;
 
 public class ElasticJobWriterTest extends Goddard {
 
@@ -32,7 +38,11 @@ public class ElasticJobWriterTest extends Goddard {
     when(es5xDao.getClient()).thenReturn(client);
     when(client.settings()).thenReturn(settings);
 
-    target = new ElasticJobWriter(es5xDao, mapper);
+    IndexRequest indexRequest = mock(IndexRequest.class);
+    when(es5xDao.bulkAdd(any(ObjectMapper.class), any(String.class), any(Object.class)))
+        .thenReturn(indexRequest);
+
+    target = new ElasticJobWriter(es5xDao, ElasticSearchPerson.MAPPER);
   }
 
   @Test
@@ -47,7 +57,9 @@ public class ElasticJobWriterTest extends Goddard {
 
   @Test
   public void write_Args__List() throws Exception {
-    List<Object> items = new ArrayList<Object>();
+    List<TestNormalizedEntity> items = new ArrayList<>();
+    items.add(new TestNormalizedEntity(DEFAULT_CLIENT_ID));
+
     target.write(items);
   }
 
