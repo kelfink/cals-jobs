18410bd32e993f5211de63f5f8e10706a9e8f2cb
David R. Smith <David.Smith@osi.ca.gov>
2017-11-20 11:10:46 -0800

    #152130766: sensitive/sealed clients

---
+++ src/main/java/gov/ca/cwds/neutron/rocket/RestrictedClientRocket.java
@@ -0,0 +1,70 @@
+package gov.ca.cwds.neutron.rocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Inject;
+
+import gov.ca.cwds.dao.cms.ReplicatedClientDao;
+import gov.ca.cwds.data.es.ElasticsearchDao;
+import gov.ca.cwds.data.persistence.cms.EsClientAddress;
+import gov.ca.cwds.jobs.ClientIndexerJob;
+import gov.ca.cwds.jobs.config.FlightPlan;
+import gov.ca.cwds.jobs.schedule.LaunchCommand;
+import gov.ca.cwds.jobs.util.jdbc.NeutronRowMapper;
+import gov.ca.cwds.neutron.atom.AtomValidateDocument;
+import gov.ca.cwds.neutron.inject.annotation.LastRunFile;
+
+/**
+ * Rocket to load sealed and sensitive Clients from CMS into ElasticSearch.
+ * 
+ * @author CWDS API Team
+ */
+public class RestrictedClientRocket extends ClientIndexerJob
+    implements NeutronRowMapper<EsClientAddress>, AtomValidateDocument {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Construct rocket with all required dependencies.
+   * 
+   * @param dao Client DAO
+   * @param esDao ElasticSearch DAO
+   * @param lastRunFile last run date in format yyyy-MM-dd HH:mm:ss
+   * @param mapper Jackson ObjectMapper
+   * @param flightPlan command line options
+   */
+  @Inject
+  public RestrictedClientRocket(final ReplicatedClientDao dao, final ElasticsearchDao esDao,
+      @LastRunFile final String lastRunFile, final ObjectMapper mapper, FlightPlan flightPlan) {
+    super(dao, esDao, lastRunFile, mapper, flightPlan);
+  }
+
+  @Override
+  public String getInitialLoadQuery(String dbSchemaName) {
+    final StringBuilder buf = new StringBuilder();
+    buf.append("SELECT x.* FROM ").append(dbSchemaName).append('.').append(getInitialLoadViewName())
+        .append(" x WHERE x.clt_identifier BETWEEN ':fromId' AND ':toId' ")
+        .append(" AND x.CLT_SENSTV_IND in ('S','R') ").append(getJdbcOrderBy())
+        .append(" FOR READ ONLY WITH UR ");
+    return buf.toString();
+  }
+
+  /**
+   * If sealed or sensitive data must NOT be loaded then any records indexed with sealed or
+   * sensitive flag must be deleted.
+   */
+  @Override
+  public boolean mustDeleteLimitedAccessRecords() {
+    return false;
+  }
+
+  /**
+   * Rocket entry point.
+   * 
+   * @param args command line arguments
+   * @throws Exception unhandled launch error
+   */
+  public static void main(String... args) throws Exception {
+    LaunchCommand.launchOneWayTrip(RestrictedClientRocket.class, args);
+  }
+
+}
