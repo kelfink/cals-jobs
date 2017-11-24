b4eff874adebdc721e9e4a5c0a920eb381143b87
David R. Smith <David.Smith@osi.ca.gov>
2017-11-24 12:47:50 -0800

    #152566679: missing cases

--- src/main/java/gov/ca/cwds/data/persistence/cms/EsChildPersonCase.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/EsChildPersonCase.java
@@ -14,14 +14,16 @@ import org.hibernate.annotations.NamedNativeQuery;
 @Table(name = "VW_LST_CASE_HIST")
 @NamedNativeQuery(
     name = "gov.ca.cwds.data.persistence.cms.EsChildPersonCase.findAllUpdatedAfterChild",
-    query = "SELECT x.* FROM (" + CaseSQLResource.SELECT_LAST_RUN_CHILD + ") x "
-        + " ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID FOR READ ONLY WITH UR ",
+    query = CaseSQLResource.SELECT_LAST_RUN_CHILD
+        // + " ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID " // DB2 won't optimize
+        + "FOR READ ONLY WITH UR ",
     resultClass = EsChildPersonCase.class, readOnly = true)
 
 @NamedNativeQuery(
     name = "gov.ca.cwds.data.persistence.cms.EsChildPersonCase.findAllUpdatedAfterParent",
     query = CaseSQLResource.SELECT_LAST_RUN_PARENT
-        + " ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID FOR READ ONLY WITH UR ",
+        // + " ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID " // DB2 won't optimize
+        + "FOR READ ONLY WITH UR ",
     resultClass = EsChildPersonCase.class, readOnly = true)
 
 @NamedNativeQuery(
