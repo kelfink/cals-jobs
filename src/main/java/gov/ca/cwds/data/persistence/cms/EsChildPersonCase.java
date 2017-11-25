b8418087cdeb7f4bb85c94ddbf5897cbea830c9c
David R. Smith <David.Smith@osi.ca.gov>
2017-11-25 12:12:53 -0800

    #152566679: missing cases

--- src/main/java/gov/ca/cwds/data/persistence/cms/EsChildPersonCase.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/EsChildPersonCase.java
@@ -31,28 +31,6 @@ import org.hibernate.annotations.NamedNativeQuery;
         + "ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID FOR READ ONLY WITH UR ",
     resultClass = EsChildPersonCase.class, readOnly = true)
 
-// @NamedNativeQuery(name =
-// "gov.ca.cwds.data.persistence.cms.EsChildPersonCase.findAllUpdatedAfter",
-// query = "SELECT c.* FROM {h-schema}VW_LST_CASE_HIST c WHERE c.CASE_ID IN ("
-// + " SELECT c1.CASE_ID FROM {h-schema}VW_LST_CASE_HIST c1 " + "WHERE c1.LAST_CHG > :after "
-// + ") ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID FOR READ ONLY WITH UR ",
-// resultClass = EsChildPersonCase.class, readOnly = true)
-//
-// @NamedNativeQuery(
-// name =
-// "gov.ca.cwds.data.persistence.cms.EsChildPersonCase.findAllUpdatedAfterWithUnlimitedAccess",
-// query = "SELECT c.* FROM {h-schema}VW_LST_CASE_HIST c WHERE c.CASE_ID IN ("
-// + " SELECT c1.CASE_ID FROM {h-schema}VW_LST_CASE_HIST c1 " + "WHERE c1.LAST_CHG > :after "
-// + ") AND c.LIMITED_ACCESS_CODE = 'N' ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID FOR READ ONLY
-// WITH UR ",
-// resultClass = EsChildPersonCase.class, readOnly = true)
-
-@NamedNativeQuery(
-    name = "gov.ca.cwds.data.persistence.cms.EsChildPersonCase.findAllUpdatedAfterWithLimitedAccess",
-    query = "SELECT c.* FROM {h-schema}VW_LST_CASE_HIST c WHERE c.CASE_ID IN ("
-        + " SELECT c1.CASE_ID FROM {h-schema}VW_LST_CASE_HIST c1 " + "WHERE c1.LAST_CHG > :after "
-        + ") AND c.LIMITED_ACCESS_CODE != 'N' ORDER BY FOCUS_CHILD_ID, CASE_ID, PARENT_ID FOR READ ONLY WITH UR ",
-    resultClass = EsChildPersonCase.class, readOnly = true)
 public class EsChildPersonCase extends EsPersonCase {
 
   private static final long serialVersionUID = 8157993904607079133L;
