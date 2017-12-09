87518f7319b9c52a17043ff6a78d71d331886616
David R. Smith <David.Smith@osi.ca.gov>
2017-12-09 13:22:18 -0800

    JIRA INT-532: missing cases

--- src/main/java/gov/ca/cwds/data/persistence/cms/StaffPerson.java
+++ src/main/java/gov/ca/cwds/data/persistence/cms/StaffPerson.java
@@ -55,7 +55,7 @@ import gov.ca.cwds.data.persistence.cms.rep.EmbeddableCmsReplicatedEntityAware;
           + "s.CNTY_SPFCD, \n"
           + "s.DTYWKR_IND, \n"
           + "s.FKCWSADDRT, \n"
-          + "trim(s.EMAIL_ADDR) AS EMAIL_ADDR\n"
+          + "trim(s.EMAIL_ADDR) AS EMAIL_ADDR,\n"
           + "s.IBMSNAP_LOGMARKER,\n"
           + "s.IBMSNAP_OPERATION\n"
         + "FROM {h-schema}STFPERST s\n FOR READ ONLY WITH UR ",
