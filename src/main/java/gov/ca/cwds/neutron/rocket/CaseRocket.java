c03b52f992fc576d9184d96b9c371b5c1e2c4701
David R. Smith <David.Smith@osi.ca.gov>
2017-12-09 13:32:57 -0800

    JIRA INT-532: missing cases

--- src/main/java/gov/ca/cwds/neutron/rocket/CaseRocket.java
+++ src/main/java/gov/ca/cwds/neutron/rocket/CaseRocket.java
@@ -627,7 +627,6 @@ public class CaseRocket extends InitialLoadJdbcRocket<ReplicatedPersonCases, EsC
 
       String json =
           ElasticSearchPerson.MAPPER.writeValueAsString(mapReadyClientCases.get("TMZGOO205B"));
-      // final String json = JsonUtils.to(mapReadyClientCases.get("TMZGOO205B"));
       LOGGER.info("Amber: {}", json);
 
       json = ElasticSearchPerson.MAPPER.writeValueAsString(mapReadyClientCases.get("TBCF40g0D8"));
