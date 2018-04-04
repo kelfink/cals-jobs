03303f7828cac3e905202e65db74e0dbc99446d5
Alexander Serbin <alserbin@gmail.com>
2018-04-04 11:20:34 -0700

    CALS-5830 split cals jobs jar into lis, cws and rfa job jars to support deployment efforts

--- jobs-cals/cws_facility_index.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call gradle clean shadowJar
-call mkdir cws-out
-java -Dlog4j.configuration=file:log4j.properties -cp build/libs/cals-jobs-0.6.2-SNAPSHOT.jar gov.ca.cwds.jobs.cals.facility.cws.CwsFacilityJobRunner ^
-     -c config/cals/facility/cws-facility-job.yaml -l ./cws-out/ ^
-     > ./cws-out/out_Facility.txt 2>&1
---
+++ jobs-facilities-common/src/test/java/gov/ca/cwds/jobs/cals/facility/AssertFacilityHelper.java
@@ -0,0 +1,36 @@
+package gov.ca.cwds.jobs.cals.facility;
+
+import static io.dropwizard.testing.FixtureHelpers.fixture;
+import static org.junit.Assert.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.dropwizard.jackson.Jackson;
+import java.util.Optional;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+/**
+ * Created by Alexander Serbin on 3/28/2018.
+ */
+public final class AssertFacilityHelper {
+
+  private AssertFacilityHelper() {
+  }
+
+  public static void assertFacility(String fixturePath, String facilityId)
+      throws JSONException, JsonProcessingException {
+    JSONAssert.assertEquals(
+        fixture(fixturePath),
+        Jackson.newObjectMapper().writeValueAsString(getFacilityById(facilityId)),
+        JSONCompareMode.STRICT);
+  }
+
+  private static ChangedFacilityDTO getFacilityById(String facilityId) {
+    Optional<ChangedFacilityDTO> optional = TestWriter.getItems().stream()
+        .filter(o -> facilityId.equals(o.getId())).findAny();
+    assertTrue(optional.isPresent());
+    return optional.orElse(null);
+  }
+
+}
