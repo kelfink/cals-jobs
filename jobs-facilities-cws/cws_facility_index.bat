a07e87a6a3299ccb79a1ef4652994e1a9e83b439
Alexander Serbin <alserbin@gmail.com>
2018-05-01 13:31:46 -0700

    CALS-5659 fixed facility index configuration to address sorting by name

--- jobs-facilities-cws/cws_facility_index.bat
+++ jobs-facilities-cws/cws_facility_index.bat
@@ -1,7 +1,7 @@
 @echo off
 call gradle clean shadowJar
 call mkdir cws-out
-java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cws-facilities-job-0.6.3-SNAPSHOT.jar ^
+java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cws-facilities-job-0.6.4-SNAPSHOT.jar ^
      -c config/cws-facility-job.yaml -l ./cws-out/ ^
      > ./cws-out/out_Facility.txt 2>&1
                                                                                                    
