b0ee881ee19d6706818805c2150f4237a7d8e89e
Alexander Serbin <alserbin@gmail.com>
2018-07-03 09:48:02 -0700

    SEAR-127 fixed version in dev script files

--- jobs-facilities-cws/unix_cws_facility_index.sh
+++ jobs-facilities-cws/unix_cws_facility_index.sh
@@ -5,6 +5,6 @@ sudo gradle shadowJar
 rm -rf cws-out
 mkdir cws-out
 
-java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cws-facilities-job-0.6.4-SNAPSHOT.jar \
+java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cws-facilities-job-1.0.1-SNAPSHOT.jar \
      -c config/unix_cws_facility-job.yaml -l ./cws-out/ \
      > ./cws-out/out_Facility.txt 2>&1
